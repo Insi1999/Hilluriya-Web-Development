f77b2583f5e12517bd6fe57f43f123aa341c9d84
Insi1999 <133994423+Insi1999@users.noreply.github.com>
2023-05-29 20:50:24 +0530

    message_mail.php

--- Hillurian Final (29.05.2023)/hilurians2/message_mail.php
+++ Hillurian Final (29.05.2023)/hilurians2/message_mail.php
@@ -29,7 +29,7 @@
         //set port to connect smtp
         $mail->Port="587";
         //set  gmail user name
-        $mail->Username="rminsimam1234@gmail.com";
+        $mail->Username="your_mail@gmail.com";
         //set email password
         $mail->Password="hrmtjevmijlnvbbj";
         //set email subject
