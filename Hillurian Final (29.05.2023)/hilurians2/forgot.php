58f72f1aab1e1345cd23a7f6d16bc0124af910c9
Insi1999 <133994423+Insi1999@users.noreply.github.com>
2023-05-29 20:49:43 +0530

    forgot.php

--- Hillurian Final (29.05.2023)/hilurians2/forgot.php
+++ Hillurian Final (29.05.2023)/hilurians2/forgot.php
@@ -118,7 +118,7 @@ include "connection.php";
 			//set port to connect smtp
 			$mail->Port="587";
 			//set  gmail user name
-			$mail->Username="rminsimam1234@gmail.com";
+			$mail->Username="your_mail@gmail.com";
 			//set email password
 			$mail->Password="hrmtjevmijlnvbbj";
 			//set email subject
