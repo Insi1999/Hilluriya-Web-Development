f91653d56bcd9fbee5938b8cbb9ef27d743001bb
Insi1999 <133994423+Insi1999@users.noreply.github.com>
2023-05-29 21:02:17 +0530

    message_mail.php

--- Hillurian Final (29.05.2023)/nfp/hilurians2/message_mail.php
+++ Hillurian Final (29.05.2023)/nfp/hilurians2/message_mail.php
@@ -29,13 +29,13 @@
         //set port to connect smtp
         $mail->Port="587";
         //set  gmail user name
-        $mail->Username="rminsimam1234@gmail.com";
+        $mail->Username="your_mail@gmail.com";
         //set email password
         $mail->Password="hrmtjevmijlnvbbj";
         //set email subject
         $mail->Subject="Reply For Your FeedBack";
         //set sender email
-        $mail->setFrom("rminsimam1234@gmail.com",'Insi');
+        $mail->setFrom("your_mail@gmail.com",'Insi');
         //using html codes
         $mail->isHTML(true);
         //attachment
