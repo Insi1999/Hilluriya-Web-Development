e29dad306798b76f7a1c60cb876306e6bdcededf
Insi1999 <133994423+Insi1999@users.noreply.github.com>
2023-05-29 20:54:54 +0530

    resendotp.php

--- Hillurian Final (29.05.2023)/hilurians2/resendotp.php
+++ Hillurian Final (29.05.2023)/hilurians2/resendotp.php
@@ -44,13 +44,13 @@ $sqlr="select * from verification where email='$email1'";
         //set port to connect smtp
         $mail->Port="587";
         //set  gmail user name
-        $mail->Username="rminsimam1234@gmail.com";
+        $mail->Username="your_mail@gmail.com";
         //set email password
         $mail->Password="hrmtjevmijlnvbbj";
         //set email subject
         $mail->Subject="Password Reset Verification Mail";
         //set sender email
-        $mail->setFrom("rminsimam1234@gmail.com",'Insi');
+        $mail->setFrom("your_mail@gmail.com",'Insi');
         //using html codes
         $mail->isHTML(true);
         //attachment
