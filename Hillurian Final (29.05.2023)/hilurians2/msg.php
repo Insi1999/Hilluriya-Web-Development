fc32f2c7313f94c8341f95428ff9c9a44950c3d9
Insi1999 <133994423+Insi1999@users.noreply.github.com>
2023-05-29 20:43:58 +0530

    msg.php

--- Hillurian Final (29.05.2023)/hilurians2/msg.php
+++ Hillurian Final (29.05.2023)/hilurians2/msg.php
@@ -65,7 +65,7 @@ use PHPMailer\PHPMailer\Exception;
        //set port to connect smtp
        $mail->Port="587";
        //set  gmail user name
-       $mail->Username="rminsimam1234@gmail.com";
+       $mail->Username="your_mail@gmail.com";
        //set email password
        $mail->Password="hrmtjevmijlnvbbj";
        //set email subject
